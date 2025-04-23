ae0af2aca1144ade95af541989cd6352170fead7
YeomJongSeop <12201911@inha.edu>
2025-04-23 13:31:00 +0900

    Title: 특정 수 도달하기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250423/특정 수 도달하기/reaching-specific-number.py
@@ -0,0 +1,10 @@
+a=list(map(int,input().split()))
+
+a_sum=0
+for i in range(len(a)):
+    a_sum+=a[i]
+    if a_sum>=260:
+        a_sum -=a[i]
+        print(a_sum,end=' ')
+        print(a_sum/i)
+        break
