473951283b664f2685b324772c68e6817fbbb3f2
YeomJongSeop <12201911@inha.edu>
2025-04-23 13:43:36 +0900

    Title: 특정 수 도달하기, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250423/특정 수 도달하기/reaching-specific-number.py
+++ 250423/특정 수 도달하기/reaching-specific-number.py
@@ -4,8 +4,10 @@ a_sum=0
 if a[0]<250:
     for i in range(len(a)):
         a_sum+=a[i]
-        if a_sum>=260:
+        if a[i]>250:
             a_sum -=a[i]
             print(a_sum,end=' ')
             print(round(a_sum/i,1))
             break
+        elif a[len(a)-1]<250:
+            print(a_sum, round(a_sum/len(a),1))
