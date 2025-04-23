b8873be9c70c6da470ea238ca6a2b8fc32c03ef4
YeomJongSeop <12201911@inha.edu>
2025-04-23 16:11:06 +0900

    Title: 특정 위치의 문자, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 250423/특정 위치의 문자/char-in-specific-location.py
+++ 250423/특정 위치의 문자/char-in-specific-location.py
@@ -5,3 +5,12 @@ arr=['L', 'E', 'B', 'R', 'O', 'S']
 for i in range(len(arr)):
     if arr[i]== N:
         print(i)
+        break
+    else: 
+        if i ==len(arr)-1:
+            print('None')
+        continue
+
+    
+
+    
