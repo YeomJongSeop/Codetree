1a651e18baddc482d18d3a9bda3aafc5ce121545
YeomJongSeop <12201911@inha.edu>
2025-04-23 13:55:20 +0900

    Title: 학점 계산기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250423/학점 계산기/credit-calculator.py
@@ -0,0 +1,13 @@
+N=int(input())
+a=list(map(float,input().split()))
+
+avg=(round(sum(a)/N,1))
+print(avg)
+
+if avg >4.0:
+    print('Perfect')
+
+elif avg>3.0:
+    print('Good')
+
+else: print('Poor')
