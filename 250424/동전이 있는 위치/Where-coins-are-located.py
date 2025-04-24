91f3f9ce5f5a6f378282c432027c95a9bdfef8ba
YeomJongSeop <12201911@inha.edu>
2025-04-24 14:21:14 +0900

    Title: 동전이 있는 위치, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250424/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,18 @@
+import sys
+input=sys.stdin.readline
+
+N,M= map(int,input().split())
+
+arr_2d=[
+    [0 for _ in range(N)] 
+    for _ in range(N)
+]
+
+for _ in range(M):
+    r,c=tuple(map(int,input().split()))
+    arr_2d[r-1][c-1]=1
+
+for row in arr_2d:
+    for elem in row:
+        print(elem,end=' ')
+    print()
