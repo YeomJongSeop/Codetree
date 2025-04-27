a2f78c78c1c6532de55af5772df3c02446f46f64
YeomJongSeop <12201911@inha.edu>
2025-04-27 15:50:18 +0900

    Title: 특정 문자의 유무, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250427/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,14 @@
+a=input()
+
+if 'ee' in a:
+    print('Yes',end=' ')
+    if 'ab' in a:
+        print('Yes')
+    else:
+        print('No')
+else:
+    print('No',end=' ')
+    if 'ab' in a:
+        print('Yes')
+    else:
+        print('No')
