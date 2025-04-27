646b688ab04000bfb14711e3beb66ef213223984
YeomJongSeop <12201911@inha.edu>
2025-04-27 16:01:22 +0900

    Title: 문자열의 특정 위치 찾기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250427/문자열의 특정 위치 찾기/find-specific-location-in-spring.py
+++ 250427/문자열의 특정 위치 찾기/find-specific-location-in-spring.py
@@ -1,8 +1,12 @@
 a=input().split()
 
-idx=-1
 
 for i in range(len(a[0])):
-    if a[1] in a[0][i]:
+    if a[0][i]==a[1]:
         print(i)
         break
+else: print('No')
+
+#if a[1] in a[0]:
+    #print(a[0].index(a[1]))
+#else: print('No')
