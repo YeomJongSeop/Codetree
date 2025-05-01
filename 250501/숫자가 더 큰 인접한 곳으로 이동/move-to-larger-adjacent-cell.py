a18a27c10cb5c23d0dc61300bbcce06cb748ff00
YeomJongSeop <12201911@inha.edu>
2025-05-01 22:09:57 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250501/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 250501/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,14 +1,8 @@
 n, r, c = map(int, input().split())
-a = []
-
-for _ in range(n):
-    row = list(map(int, input().split()))
-    a.append(row)
-
+a = [ list(map(int, input().split())) for _ in range(n)]
 
 
 x,y=r-1,c-1 # 시작점
-
 
 def in_range(x,y):
     return 0<=x and x<n and 0<=y and y<n
@@ -18,8 +12,32 @@ def in_range(x,y):
 dxs=[-1,1,0,0]
 dys=[0,0,-1,1]
 
+def simulate(x, y):
+    dxs = [-1, 1, 0, 0]  # 상 하 좌 우
+    dys = [0, 0, -1, 1]
+    max_val = a[x][y]
+
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and a[nx][ny] > max_val:
+            return nx, ny  # 더 큰 값이 있으면 이동 좌표 반환
+
+    return None  # 이동 불가능할 경우
 
 
+path = [a[x][y]]
+
+while True:
+    result = simulate(x, y)
+    if result is None:
+        break  # 더 이상 이동 불가능
+    x, y = result  # 새로운 위치로 이동
+    path.append(a[x][y])
+
+print(*path)
+
+
+'''
 def simulate():
     path=[a[x][y]]
     cur_x,cur_y=x,y
@@ -43,7 +61,7 @@ def simulate():
 
 
 print(*simulate()) # *쓰면 리스트 표시없이 원소만 공백으로 나옴!
-
+'''
 
 
 
