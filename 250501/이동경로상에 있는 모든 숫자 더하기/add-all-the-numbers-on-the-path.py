dc6ce4f395a0a7cef619a7e2715ee93ce5117511
YeomJongSeop <12201911@inha.edu>
2025-05-01 14:34:49 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 90ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250501/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -0,0 +1,34 @@
+N, T = map(int, input().split())
+cmd = input()
+answer = [list(map(int, input().split())) for _ in range(N)]
+
+# Please write your code here.
+#격자니까 행렬 동 남 서 북 
+dxs=[0,1,0,-1]#row
+dys=[1,0,-1,0]#column
+s=[]
+
+# 북쪽
+dir_num=3
+x,y = int(N/2) , int(N/2)
+s.append(answer[x][y])
+
+def in_range(x,y):
+    return 0<=x and x<N and 0<=y and y<N
+
+for a in cmd:
+    if a =='R':
+        dir_num= (dir_num+1)%4
+    elif a =='L':
+        dir_num=(dir_num-1 +4)%4
+    elif a=='F':
+        nx,ny= x+dxs[dir_num],y+dys[dir_num]
+        if not in_range(ny,ny):
+            continue
+        x,y= x+dxs[dir_num],y+dys[dir_num]
+        s.append(answer[x][y])
+
+print(sum(s))
+
+
+
