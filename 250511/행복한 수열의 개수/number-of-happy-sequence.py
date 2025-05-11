0ad1e4d9a23dcbb484750dcf385f0ed8b649db7f
YeomJongSeop <12201911@inha.edu>
2025-05-11 15:49:52 +0900

    Title: 행복한 수열의 개수, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250511/행복한 수열의 개수/number-of-happy-sequence.py
+++ 250511/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,10 +1,10 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-def is_happy(seq, m):
+def is_happy(sequence, m):
     count = 1
-    for i in range(1, len(seq)):
-        if seq[i] == seq[i - 1]:
+    for i in range(1, len(sequence)):
+        if sequence[i] == sequence[i - 1]:
             count += 1
         else:
             count = 1
@@ -12,17 +12,17 @@ def is_happy(seq, m):
             return True
     return False
 
-happy = 0
+happy_count = 0
 
 # 행 검사
 for row in grid:
     if is_happy(row, m):
-        happy += 1
+        happy_count += 1
 
 # 열 검사
 for j in range(n):
     col = [grid[i][j] for i in range(n)]
     if is_happy(col, m):
-        happy += 1
+        happy_count += 1
 
-print(happy)
+print(happy_count)
