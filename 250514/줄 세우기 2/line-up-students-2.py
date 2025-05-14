6392ad01ce679bebcdadf3d91c448034af11d1c9
YeomJongSeop <12201911@inha.edu>
2025-05-14 12:50:36 +0900

    Title: 줄 세우기 2, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 250514/줄 세우기 2/line-up-students-2.py
+++ 250514/줄 세우기 2/line-up-students-2.py
@@ -6,5 +6,7 @@ for i in range(n):
     row=tuple(map(int,input().split())) + (i+1,)  # (i+1,) 1개의 원소를 가진 튜플(tuple)
     li.append(row)
 
+li.sort(key= lambda x:(x[0],-x[1]))
+
 for elem in li:
     print(*elem)
