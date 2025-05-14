1c513de492cc542e8983b8a5bcf3ae78eccd2c4f
YeomJongSeop <12201911@inha.edu>
2025-05-14 12:10:14 +0900

    Title: 줄 세우기, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250514/줄 세우기/line-up-students.py
+++ 250514/줄 세우기/line-up-students.py
@@ -6,6 +6,5 @@ students = [tuple(map(int, input().split())) + (i + 1,) for i in range(n)]
 students.sort(key=lambda x: (x[0],x[1]),reverse= True)
 
 
-for idx,student in enumerate(students,start=1):
+for student in students:
     print(*student)
-
