7f3f2d1a99371de7dae1b7fdce6a38cf78d7ddbd
YeomJongSeop <12201911@inha.edu>
2025-05-17 16:13:04 +0900

    Title: 신기한 타일 뒤집기, Time: 101ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250517/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 250517/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,22 +1,28 @@
 n = int(input())
 commands = [tuple(input().split()) for _ in range(n)]
-x = []
-direc = []
 
 
 # Please write your code here.
-black=[0 for _ in range(100)]
-white=[0 for _ in range(100)]
+white=[0 for _ in range(2000)] 
+black=[0 for _ in range(2000)]
+
+idx=1000
+
 
 for elem in commands:
+    num= int(elem[0])
     if elem[1] =='R':
-        for i in range(int(elem[0])):
+        for i in range(idx,idx+num):
             black[i]=1
             white[i]=0
-    else:
-        for i in range(int(elem[0])):
+        idx=idx+num-1
+
+            
+    else: # 좌측
+        for i in range(idx,idx-num,-1):
             white[i]=1
             black[i]=0
+        idx=idx-num+1
 
 
 print(sum(white),sum(black))
