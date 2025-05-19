864e567adc2e778bc54807f1bac5e62833e90013
YeomJongSeop <12201911@inha.edu>
2025-05-19 18:34:44 +0900

    Title: 벌금은 누구에게, Time: 81ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250519/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,12 @@
+N,M,K = map(int, input().split())
+arr= [int(input()) for _ in range(M)]
+
+# 학생 번호 별 벌칙 횟수 
+stu_cnt =[0 for _ in range(N+1)] 
+
+for i in range(len(arr)):
+    stu_cnt[arr[i]]+=1
+
+    if stu_cnt[arr[i]] ==K:
+        print(arr[i])
+        break
