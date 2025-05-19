36f8214bdd5031da9c01b4702ceeb1c3098b5984
YeomJongSeop <12201911@inha.edu>
2025-05-19 18:11:04 +0900

    Title: 만나는 그 순간, Time: 123ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250519/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,48 @@
+N,M = map(int,input().split())
+
+A_move=[ tuple(input().split()) for _ in range(N) ]
+B_move=[ tuple(input().split()) for _ in range(M) ]
+
+A_pos=[0 for _ in range(1000)]
+B_pos=[0 for _ in range(1000)]
+
+idx=500
+time=0
+
+for d,move in A_move:
+    if d == 'R':
+        for t in range(1,int(move)+1):
+            idx+=1
+            A_pos[time+t] = idx
+
+
+    else:
+        for t in range(1,int(move)+1):
+            idx-=1
+            A_pos[time+t] = idx
+    time+= int(move)
+
+
+idx=500
+time=0
+
+for d,move in B_move:
+    if d == 'R':
+        for t in range(1,int(move)+1):
+            idx+=1
+            B_pos[time+t] = idx
+
+
+    else:
+        for t in range(1,int(move)+1):
+            idx-=1
+            B_pos[time+t] = idx
+    time+= int(move)
+
+
+for t in range(1,time+1):
+    if A_pos[t] == B_pos[t]:
+        print(t)
+        break
+
+else: print(-1)
