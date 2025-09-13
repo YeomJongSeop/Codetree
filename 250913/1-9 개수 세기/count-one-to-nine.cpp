eac41a263dbdb30184558675014eae1dbe5bee95
YeomJongSeop <12201911@inha.edu>
2025-09-13 17:35:02 +0900

    Title: 1-9 개수 세기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250913/1-9 개수 세기/count-one-to-nine.cpp
+++ 250913/1-9 개수 세기/count-one-to-nine.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main() {
     // Please write your code here.
     int count[10]={};
-    int arr[10];
+    int arr[100];
     int n;
 
     cin>>n;
 
     for(int i=0;i<n;i++){
         cin>>arr[i];
+        
         count[arr[i]]++;
     }
     
