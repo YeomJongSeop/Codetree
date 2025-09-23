6f8e3bf93c3ac73a00476478f53b94bc9bbb32b0
YeomJongSeop <12201911@inha.edu>
2025-09-23 23:37:48 +0900

    Title: 대문자로 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250923/대문자로 출력하기/print-in-capital.cpp
+++ 250923/대문자로 출력하기/print-in-capital.cpp
@@ -7,7 +7,7 @@ int main() {
     // Please write your code here.
     string str;
     cin>>str;
-    
+
     string ans;
 
 
@@ -24,3 +24,34 @@ int main() {
 
     return 0;
 }
+
+/*
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    // 문자열을 정의합니다.
+    string str;
+    
+    // 문자열을 입력받습니다.
+    cin >> str;
+    
+    // 문자열의 길이를 구합니다.
+    int len = str.length();
+    
+    // 문자를 하나하나 확인하여 알파벳일 경우 모두 대문자로 출력합니다.
+    for(int i = 0; i < len; i++) {
+        if((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) {
+            cout << (char)toupper(str[i]);
+        }
+    }
+    
+    return 0;
+}
+
+
+
+
+*/
