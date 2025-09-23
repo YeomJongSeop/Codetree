15bd4d5c8601f31209e3e020ce7d5d705dbb6513
YeomJongSeop <12201911@inha.edu>
2025-09-23 22:54:04 +0900

    Title: a로 채워넣기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250923/a로 채워넣기/filling-with-a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >>str;
+
+    str[1] = 'a';
+    str[str.size()-2] = 'a';
+
+    cout<<str;
+
+
+    return 0;
+}
