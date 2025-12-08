e326ec58c54bf65fbcdbe2d9f218312f037e6181
YeomJongSeop <12201911@inha.edu>
2025-12-08 14:08:36 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251208/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
+++ 251208/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -1,18 +1,23 @@
+
 #include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    int n,m;
-    cin>>n>>m;
+void print_predict(int n,int m){
     for(int i=0; i<n; i++){
 	    for(int j=0; j<m;j++){
 	        cout<<"1";
 	    }
 	cout<<endl;
     }
-	
+}
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    print_predict(n,m);
     // Please write your code here.
     return 0;
 }
+
