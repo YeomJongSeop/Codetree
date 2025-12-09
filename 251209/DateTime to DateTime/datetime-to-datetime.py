e74261df6efbf85ebc89a0606107a31ff856f2e1
YeomJongSeop <12201911@inha.edu>
2025-12-09 11:50:50 +0900

    Title: DateTime to DateTime, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251209/DateTime to DateTime/datetime-to-datetime.py
+++ 251209/DateTime to DateTime/datetime-to-datetime.py
@@ -1,15 +1,12 @@
 A,B,C = map(int,input().split())
-day=11
-hour=11
-minute=11
+
 ans=0
 
 if (A, B, C) < (11, 11, 11):
     print(-1)
 
 else:
-    if A>=11 and B>=11 and C>=11:
-        ans=(((A-11)*60*24) + ((B-11)*60)+(C-11))
-        print(ans)
+    ans=(((A-11)*60*24) + ((B-11)*60)+(C-11))
+    print(ans)
         
 
