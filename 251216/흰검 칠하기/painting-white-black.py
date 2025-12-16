9977e4a43ab17975cdc65a75332e5da6a9420663
YeomJongSeop <12201911@inha.edu>
2025-12-16 14:55:30 +0900

    Title: 흰검 칠하기, Time: 84ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251216/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,44 @@
+N = int(input())
+li = [list(input().split()) for _ in range(N)]
+
+offset = 500
+
+# 0: no 1: white 2:black 3: gray
+state = [0] * 1000
+
+b_cnt = [0] * 1000
+w_cnt = [0] * 1000
+
+cur = offset
+
+for x, y in li:
+    x=int(x)
+    if y == 'R':  # 검정
+        nxt = cur + x-1
+        for i in range(cur, nxt+1):
+            b_cnt[i] += 1
+            state[i] = 2
+        cur = nxt
+
+    elif y == 'L':
+        nxt = cur - x+1
+        for i in range(nxt, cur+1):
+            w_cnt[i] += 1
+            state[i] = 1
+        cur = nxt
+
+w = 0
+b = 0
+g = 0
+
+for i in range(len(state)):
+    if w_cnt[i] >= 2 and b_cnt[i] >= 2:
+        state[i] = 3
+    if state[i] == 1:
+        w += 1
+    elif state[i] == 2:
+        b += 1
+    elif state[i] == 3:
+        g += 1
+
+print(w, b, g)
