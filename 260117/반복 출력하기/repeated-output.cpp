b794d418fc4270cb6b97f067e211603b9de9b5de
YeomJongSeop <12201911@inha.edu>
2026-01-17 15:38:02 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260117/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void Printstr(int n){
+    for(int i=0; i<n; i++){
+        cout<<"12345^&*()_"<<endl;
+    }
+
+
+}
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    Printstr(n);
+
+    return 0;
+}
